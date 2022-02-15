37b580d4d879c2ecb2436cae7ce5c498ddfb0af9
fyky <91657176+fyky@users.noreply.github.com>
2022-02-15 22:51:11 +0900

    Add files via upload
    配列の取り出し方

---
+++ test2.rb
@@ -0,0 +1,18 @@
+# @test = [[{"a"=>"x"},{"a"=>"y"}],[{"a"=>"x"},{"a"=>"y"}],[{"a"=>"x"},{"a"=>"y"}],[{"a"=>"x"},{"a"=>"y"}]]   
+   
+# # p @test[0].size
+# #　要素の数
+#     for f in 0..(@test[0].size-1) do
+#       for val in @test do
+#         p val[f]["a"]
+#       end
+#     end
+
+i=0
+while i <= 2 do
+  # for i in 1..100 do
+    i+= 1
+    p i
+    # result << JSON.parse(uri.read)
+  end
+
